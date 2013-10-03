09e73e13396f30db0dd8305f78cafa10ed31bc37
Jason Rogena <jasonrogena@gmail.com>
2013-10-03 16:30:19 +0300

    n2_prices table has been added to the database

--- database/nitrogen_acq_.sql
+++ database/nitrogen_acq_.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 30, 2013 at 06:54 AM
+-- Generation Time: Oct 03, 2013 at 04:28 PM
 -- Server version: 5.5.32-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.3
 
@@ -31,56 +31,44 @@ CREATE TABLE IF NOT EXISTS `acquisitions` (
   `user_id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `amount_req` int(11) NOT NULL,
-  `amount_appr` int(11) DEFAULT NULL,
+  `amount_req` float NOT NULL,
+  `amount_appr` float DEFAULT NULL,
+  `added_by` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`,`project_id`),
   KEY `project_id` (`project_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `acquisitions`
 --
 
-INSERT INTO `acquisitions` (`id`, `user_id`, `project_id`, `date`, `amount_req`, `amount_appr`) VALUES
-(2, 2, 3, '2012-08-07', 3, NULL),
-(3, 3, 4, '2012-08-08', 6, NULL),
-(5, 5, 6, '2012-08-08', 1, NULL),
-(7, 6, 3, '2012-08-09', 3, NULL),
-(8, 7, 7, '2012-08-10', 3, NULL),
-(11, 2, 3, '2012-08-14', 3, NULL),
-(12, 6, 3, '2012-08-14', 3, NULL),
-(13, 10, 6, '2012-08-15', 3, NULL),
-(14, 4, 8, '2012-08-09', 2, NULL),
-(15, 4, 8, '2012-08-09', 3, NULL),
-(16, 8, 9, '2012-08-10', 3, NULL),
-(17, 8, 9, '2012-08-16', 3, NULL),
-(18, 11, 10, '2012-08-16', 1, NULL),
-(19, 12, 11, '2012-08-17', 10, NULL),
-(20, 6, 3, '2012-08-17', 13, NULL),
-(21, 13, 12, '2012-08-21', 1, NULL),
-(22, 14, 13, '2012-08-22', 1, NULL),
-(23, 8, 9, '2012-08-22', 3, NULL),
-(24, 15, 14, '2012-08-22', 2, NULL),
-(25, 16, 15, '2012-08-23', 10, NULL),
-(26, 5, 6, '2012-08-23', 3, NULL),
-(27, 17, 6, '2012-08-24', 3, NULL),
-(28, 7, 12, '2012-08-24', 2, NULL),
-(29, 18, 16, '2012-08-24', 5, NULL),
-(30, 14, 13, '2012-08-27', 1, NULL),
-(31, 19, 4, '2012-08-28', 35, NULL),
-(32, 14, 13, '2012-08-28', 1, NULL),
-(33, 14, 13, '2012-08-29', 1, NULL),
-(34, 20, 17, '2012-08-29', 3, NULL),
-(35, 14, 13, '2012-08-31', 1, NULL),
-(36, 21, 12, '2012-09-02', 3, NULL),
-(37, 14, 13, '2012-09-04', 2, NULL),
-(38, 20, 17, '2012-09-05', 3, NULL),
-(39, 20, 17, '2012-09-06', 1, NULL),
-(40, 22, 18, '2012-09-06', 3, NULL),
-(41, 11, 10, '2012-09-06', 6, NULL),
-(42, 23, 19, '2012-09-06', 1, NULL),
-(43, 14, 13, '2012-09-07', 2, NULL);
+INSERT INTO `acquisitions` (`id`, `user_id`, `project_id`, `date`, `amount_req`, `amount_appr`, `added_by`) VALUES
+(14, 115, 82, '2013-10-03', 2, 34, 'jrogena'),
+(15, 116, 83, '2013-10-03', 1, 2, 'jrogena'),
+(16, 116, 84, '2013-08-13', 332, 43, 'jrogena'),
+(17, 115, 84, '2013-10-03', 50, 32, 'jrogena');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ln2_prices`
+--
+
+CREATE TABLE IF NOT EXISTS `ln2_prices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` float NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `ln2_prices`
+--
+
+INSERT INTO `ln2_prices` (`id`, `price`, `start_date`, `end_date`) VALUES
+(1, 1.1, '2013-10-01', '2014-07-15');
 
 -- --------------------------------------------------------
 
@@ -94,29 +82,16 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `charge_code` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `charge_code` (`charge_code`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=85 ;
 
 --
 -- Dumping data for table `projects`
 --
 
 INSERT INTO `projects` (`id`, `name`, `charge_code`) VALUES
-(3, 'TIITA', 'TIITA-051'),
-(4, 'TIITA', 'TIITA-041'),
-(6, 'TIITA', 'TIITA-044'),
-(7, 'TCIMM', 'TCIMM-1T-02'),
-(8, 'BC01, COR', 'BC01-NB0-SWE-010, COR-SWE-01003'),
-(9, 'BC01, COR', 'BC01-NB0-SWE-010, COR-SWE-01015'),
-(10, 'BC01, COR', 'BC01-NB0-SWE-010, COR-SWE-1009'),
-(11, 'Beca', 'Beca-12'),
-(12, 'TCIMMYT', 'TCIMMYT-02'),
-(13, 'BC01, COR', 'BC01-NB0-SWE-010, COR-SWE-01001'),
-(14, 'BC02', 'BC02-SERVIC'),
-(15, 'BT03', 'BT03-NB0-HP1001'),
-(16, 'Beca', 'Beca-46'),
-(17, 'Cip Beca', 'Cip Beca'),
-(18, 'BC01', 'BC01-NB0-ATL'),
-(19, 'BC01, COR', 'BC01-NB0-SWE-010, COR-SWE-1004');
+(82, 'test5', 'test5'),
+(83, 'test1', 'test1'),
+(84, 'Biorepository', 'bio-1123-21');
 
 -- --------------------------------------------------------
 
@@ -128,35 +103,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=117 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`) VALUES
-(2, 'Joshua'),
-(3, 'Arthur'),
-(4, 'Donfagsiteli'),
-(5, 'Sarah'),
-(6, 'Anne'),
-(7, 'Bridgit'),
-(8, 'Cecile Annie'),
-(9, 'James Mbora'),
-(10, 'Esther'),
-(11, 'Dora'),
-(12, 'Moses Njahira'),
-(13, 'Jacky'),
-(14, 'Benius Tukahinua'),
-(15, 'Francis'),
-(16, 'Charity Muteti'),
-(17, 'Ren Mburu'),
-(18, 'Tony Maritim'),
-(19, 'Teddy Amuga'),
-(20, 'Quinata Bukani'),
-(21, 'Maureen'),
-(22, 'James Wainaina'),
-(23, 'Eric');
+(115, 'Jason Rogena'),
+(116, 'Grace Rogena');
 
 --
 -- Constraints for dumped tables
