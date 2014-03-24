9716cf5025effe9d0679bda07c3c41b0fd1cc606
Jason Rogena <j.rogena@cgiar.org>
2014-03-24 12:31:19 +0300

    reformatting Box Storage backend to point to azizi_lims

--- modules/mod_box_storage.php
+++ modules/mod_box_storage.php
@@ -429,14 +429,14 @@ class BoxStorage extends Repository{
       $columns = array("box_name","size","box_type","location","rack","rack_position");
       $columnValues = array($_POST['box_label'], $boxSizeInLIMS, "box", $_POST['sector'], $_POST['rack'], $_POST['position']);
       $this->Dbase->CreateLogEntry('About to insert the following row of data to boxes table -> '.print_r($columnValues, true), 'debug');
-      $result = $this->Dbase->InsertOnDuplicateUpdate(Config::$config['lims_extension'].".boxes_def", $columns, $columnValues);
+      $result = $this->Dbase->InsertOnDuplicateUpdate(Config::$config['azizi_db'].".boxes_def", $columns, $columnValues);
       if($result !== 0) {
          $boxId = $result;
          //insert extra information in lims_extension database
          $columns = array("box_id","status", "features", "sample_type");
          $columnValues = array($boxId, $_POST['status'], $_POST['features'], $_POST['sample_types']);
          $this->Dbase->CreateLogEntry('About to insert the following row of data to boxes table -> '.print_r($columnValues, true), 'debug');
-         $result = $this->Dbase->InsertOnDuplicateUpdate("boxes", $columns, $columnValues);
+         $result = $this->Dbase->InsertOnDuplicateUpdate(Config::$config['lims_extension'].".boxes_def", $columns, $columnValues);
          if($result === 0){
             $message = "Unable to add some information from the last request";
             $this->Dbase->CreateLogEntry('mod_box_storage: Unable to make the last insertBox request. Last thrown error is '.$this->Dbase->lastError, 'fatal');//used fatal instead of warning because the dbase file seems to only use the fatal log
