76ee6f45aaa3e5deb291a9c8a88a01b2aa9c7a51
Jason Rogena <j.rogena@cgiar.org>
2014-04-11 15:41:29 +0300

    Changing how jqxgrid looks like in box storage module

--- js/box_storage.js
+++ js/box_storage.js
@@ -57,11 +57,11 @@ var BoxStorage = {
             theme: theme,
             pageable: true,
             columns: [
-               {text: 'Box Label', datafield: 'box_name', width: 130},
-               {text: 'Tank Position', datafield: 'position', width: 210},
+               {text: 'Box Label', datafield: 'box_name', width: 250},
+               {text: 'Tank Position', datafield: 'position', width: 300},
                {text: 'Status', datafield: 'status', width: 90},
-               {text: 'Date Added', datafield: 'date_added', width: 90},
-               {text: 'Added By', datafield: 'added_by', width: 180}
+               {text: 'Date Added', datafield: 'date_added', width: 120},
+               {text: 'Added By', datafield: 'added_by', width: 200}
             ]
          });
       }
