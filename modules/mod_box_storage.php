19b94023efcc252531540aaa2125a8343e9c446d
Jason Rogena <j.rogena@cgiar.org>
2014-04-13 12:16:37 +0300

    Implementing paging for the jqxgrid in box storage module

--- modules/mod_box_storage.php
+++ modules/mod_box_storage.php
@@ -1065,11 +1065,12 @@ class BoxStorage extends Repository{
     */
    private function searchBoxes() {
       $fromRow = $_POST['pagenum'] * $_POST['pagesize'];
+      $pageSize = $_POST['pagesize'] + 1;
       $query = 'select a.box_id, a.status, date(a.date_added) as date_added, b.box_features, b.box_name, b.keeper, b.size, b.rack, b.rack_position, c.id as sector_id, c.facility_id as tank_id , concat(c.facility, " >> ", b.rack, " >> ", b.rack_position) as position '.
               'from '. Config::$config['dbase'] .'.lcmod_boxes_def as a '.
               'inner join '. Config::$config['azizi_db'] .'.boxes_def as b on a.box_id = b.box_id './/optimization: use inner join to fetch only boxes in LN2 tanks and not the freezers etc
               'left join '. Config::$config['azizi_db'] .'.boxes_local_def as c on b.location = c.id '.
-              'limit '.$fromRow.','.$_POST['pagesize'];//fetch all boxes regardless of wether sector (boxes_local_def) is defined or not
+              'limit '.$fromRow.','.$pageSize;//fetch all boxes regardless of wether sector (boxes_local_def) is defined or not
       
       /*
        * Optimization:
