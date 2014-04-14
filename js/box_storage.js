44e7d8da6e453a617a80bf09cbb09a8d3e7cbc9c
Jason Rogena <j.rogena@cgiar.org>
2014-04-14 16:09:24 +0300

    Caching tank data in a cookie in box storage module

--- js/box_storage.js
+++ js/box_storage.js
@@ -1321,26 +1321,20 @@ var BoxStorage = {
       }
    },
    
-   setCookie: function(name, value, daysToExpire){
-      var date = new Date();
-      date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
-      var expires = "expires="+date.toGMTString();
-      document.cookie = name + "=" + value + "; " + expires + "; path=/";
-      console.log("saved cookie "+name+" as "+value);
-      console.log("gotten cookie = " +BoxStorage.getCookie(name));
+   setCookie: function(cname,cvalue,exdays){
+      var d = new Date();
+      d.setTime(d.getTime()+(exdays*24*60*60*1000));
+      var expires = "expires="+d.toGMTString();
+      document.cookie = cname + "=" + cvalue + "; " + expires;
    },
    
-   getCookie: function(name){
-      name = name + "=";
-      var allCookies = document.cookie.split(';');
-      for(var i=0; i<allCookies.length; i++) {
-         var c = allCookies[i].trim();
-         if (c.indexOf(name)===0) {
-            var cookieValue = c.substring(name.length,c.length);
-            console.log("cookie "+name+" = "+cookieValue);
-            return cookieValue;
-         }
+   getCookie: function(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0; i<ca.length; i++) {
+         var c = ca[i].trim();
+         if (c.indexOf(name)==0) return c.substring(name.length,c.length);
       }
       return -1;
-   }
+}
 };
