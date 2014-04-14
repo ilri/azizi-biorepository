8d4471a0323138bbe961cc01587c2d9caf89d255
Jason Rogena <j.rogena@cgiar.org>
2014-04-14 13:21:22 +0300

    Search results now being updated when something changes in search page of search module

--- modules/mod_box_storage.php
+++ modules/mod_box_storage.php
@@ -534,7 +534,7 @@ class BoxStorage extends Repository{
       $("#search").keyup( function(event) {
          console.log(event.which);
       }
-      /*$("#search").live("keypress", function(e) {
+      $("#search").live("keypress", function(e) {
          console.log(e.keyCode);
         if (e.keyCode == 13) {//enter key pressed
            BoxStorage.searchForBox();
@@ -544,7 +544,7 @@ class BoxStorage extends Repository{
                BoxStorage.searchForBox();
             }
         }
-      });*/
+      });
       $("#search_project").change(function (){
          BoxStorage.searchForBox();
       });
