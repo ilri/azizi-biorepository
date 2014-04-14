e6f84654785b3ef7632eeb46b3bea094e63e704a
Jason Rogena <j.rogena@cgiar.org>
2014-04-14 16:10:31 +0300

    Caching tank data in a cookie in box storage module

--- js/box_storage.js
+++ js/box_storage.js
@@ -1325,7 +1325,7 @@ var BoxStorage = {
       var d = new Date();
       d.setTime(d.getTime()+(exdays*24*60*60*1000));
       var expires = "expires="+d.toGMTString();
-      document.cookie = cname + "=" + cvalue + "; " + expires;
+      document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
    },
    
    getCookie: function(cname) {
