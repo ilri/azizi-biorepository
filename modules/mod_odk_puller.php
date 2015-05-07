f5f88ddfc728c2484b4799b61f45640e207524ba
Jason Rogena <j.rogena@cgiar.org>
2015-05-07 19:05:51 +0300

    pure JSON in ODK Puller
    Modify ODK Puller to output only pure json and not CSVs in JSON
    objects
    
    Signed-off-by: Jason Rogena <j.rogena@cgiar.org>

--- modules/mod_odk_puller.php
+++ modules/mod_odk_puller.php
@@ -22,7 +22,7 @@ class ODKPuller {
          $returnObject = array("forms" => $forms);
          echo json_encode($returnObject);
       }
-      else if($request === "get_csv"){//user wants the csv corresponding to a form
+      else if($request === "get_data"){//user wants the csv corresponding to a form
          $returnObject = array("files" => array());
          $requestedForm = $_GET['form'];
          $query = "select id from odk_forms where form_name = :form_name order by id desc limit 1";
@@ -44,58 +44,61 @@ class ODKPuller {
       $preloads = array();//an associative array with all preload file names as child elements
       $query = "select id, name from odk_preloads where form_id = :formId";
       $result = $this->database->ExecuteQuery($query, array("formId" => $formId));
-      foreach($result as $currResult) {
-         $preloads[$currResult['name']] = "";
-         $keys = array();
-         $values = array();
-         $query = "select query from preload_queries where preload_id = :id";
-         $queries = $this->database->ExecuteQuery($query, array("id" => $currResult['id']));
-         foreach($queries as $currQuery) {
-            $actualQuery = $currQuery['query'];
-            $queryRes = $this->database->ExecuteQuery($actualQuery);
-            if(is_array($queryRes) && count($queryRes) > 0) {
-               $currKeys = array_keys($queryRes[0]);
-               array_merge($values, $queryRes);
-               foreach($currKeys as $currKey) {
-                  if(!in_array($currKey, $keys)) {
-                     array_push($keys, $currKey);
+      if($_GET['complete'] == 0 || $_GET['complete'] == '0'){//just get the names of the preload files
+         foreach ($result as $currResult) {
+            $preloads[] = $currResult['name'];
+         }
+      }
+      else {
+         foreach($result as $currResult) {
+            $preloads[$currResult['name']] = "";
+            $keys = array();
+            $values = array();
+            $query = "select query from preload_queries where preload_id = :id";
+            $queries = $this->database->ExecuteQuery($query, array("id" => $currResult['id']));
+            foreach($queries as $currQuery) {
+               $actualQuery = $currQuery['query'];
+               $queryRes = $this->database->ExecuteQuery($actualQuery);
+               if(is_array($queryRes) && count($queryRes) > 0) {
+                  $currKeys = array_keys($queryRes[0]);
+                  $values = array_merge($values, $queryRes);
+                  foreach($currKeys as $currKey) {
+                     if(!in_array($currKey, $keys)) {
+                        array_push($keys, $currKey);
+                     }
                   }
                }
             }
+            $preloads[$currResult['name']] = $this->formatValues($keys, $values);
          }
-         $preloads[$currResult['name']] = $this->generateCSV($keys, $values);
       }
       return $preloads;
    }
 
    /**
-    * This function generates a csv string based on the provided data
+    * This function formats the provided values based on the keys found with the
+    * assumption that most rows in the $values array will not have all the keys 
+    * defined in $keys
     * 
-    * @param Array $keys   An array of all the expected column titles
+    * @param Array $keys   An array of all the expected keys
     * @param Array $values A multidimensional array with first level being indexes
-    *                      of rows and the second level being column headings
-    *                      corresponding to those in $keys
+    *                      of rows and the second level being keys corresponding
+    *                      to those in $keys
     * 
     * @return A the CSV string corresponding to the provided data
     */
-   private function generateCSV($keys, $values) {
-      $csv = implode(",", $keys);
-      foreach($values as $currValue) {
-         $csv .= "\n";
-         $currRow = "";
-         $keyIndex = 1;
+   private function formatValues($keys, $values) {
+      $formatted = array();
+      for($index = 0; $index < count($values); $index++) {
+         $formatted[$index] = array();
          foreach($keys as $currKey) {
-            if(isset($currValue[$currKey])){
-               $currRow .= $currValue[$currKey];
+            if(isset($values[$index][$currKey])){
+               $formatted[$index][$currKey] = $values[$index][$currKey];
             }
-            if($keyIndex < count($keyIndex)){//not yet in the last column in the row
-               $currRow .=",";
-            }
-            $keyIndex++;
+            else $formatted[$index][$currKey] = null;
          }
-         $csv .= $currRow;
       }
-      return $csv;
+      return $formatted;
    }
 }
 ?>
