572ff3d0d7ebc35d3276a7cf0ce980a8507aa33a
Jason Rogena <j.rogena@cgiar.org>
2015-06-08 11:17:07 +0300

    leaflet.js in bower
    Move references to Leaflet.js libraries to the ones deployed in
    bower
    
    Signed-off-by: Jason Rogena <j.rogena@cgiar.org>

--- modules/mod_visualize_samples.php
+++ modules/mod_visualize_samples.php
@@ -610,17 +610,17 @@ class VisualizeSamples {
    
    private function home(){
 ?>
-<link rel="stylesheet" href="<?php echo OPTIONS_COMMON_FOLDER_PATH;?>leaflet/leaflet.css" />
-<script src="<?php echo OPTIONS_COMMON_FOLDER_PATH;?>leaflet/leaflet.js"></script>
+<link rel="stylesheet" href="<?php echo OPTIONS_COMMON_FOLDER_PATH;?>bower/leaflet/dist/leaflet.css" />
+<script src="<?php echo OPTIONS_COMMON_FOLDER_PATH;?>bower/leaflet/dist/leaflet.js"></script>
 <script type='text/javascript' src='<?php echo OPTIONS_COMMON_FOLDER_PATH;?>bower/jquery/jquery.min.js' /></script>
 <script src='js/visualize_samples.js'></script>
-<script src='<?php echo OPTIONS_COMMON_FOLDER_PATH;?>leaflet/ol.js'></script>
+<!--script src='<?php echo OPTIONS_COMMON_FOLDER_PATH;?>leaflet/ol.js'></script-->
 <script src="<?php echo OPTIONS_COMMON_FOLDER_PATH;?>bower/dygraphs/dygraph-combined.js"></script>
 <link href="css/repository.css" rel="stylesheet" type="text/css" />
 <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css' />
 <link rel='stylesheet' type='text/css' href='<?php echo OPTIONS_COMMON_FOLDER_PATH ?>bower/jquery-ui/themes/smoothness/jquery-ui.min.css' />
 <script type='text/javascript' src="<?php echo OPTIONS_COMMON_FOLDER_PATH; ?>bower/jquery-ui/jquery-ui.min.js" /></script>
-<script src='<?php echo OPTIONS_COMMON_FOLDER_PATH;?>leaflet/leaflet-heat.js'></script>
+<script src='<?php echo OPTIONS_COMMON_FOLDER_PATH;?>bower/Leaflet.heat/dist/leaflet-heat.js'></script>
 <div id="map"></div>
 <div id="samples_timeline"></div>
 <div id="loading_box">Loading</div>
